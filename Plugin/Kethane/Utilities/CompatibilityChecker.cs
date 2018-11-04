550c429971bea54464e9c84a3ff4b844b8dd7fb6
Bill Currie <bill@taniwha.org>
2018-11-04 21:05:41 +0900

    Version check update.
    Allow all of KSP 1.5. I doubt Unity will be changed for a patch.

--- Plugin/Kethane/Utilities/CompatibilityChecker.cs
+++ Plugin/Kethane/Utilities/CompatibilityChecker.cs
@@ -64,7 +64,8 @@ namespace Kethane.Utilities
             // Even if you don't lock down functionality, you should return true if your users
             // can expect a future update to be available.
             //
-            return Versioning.Revision == 4 && Versioning.version_minor == 4 && Versioning.version_major == 1;
+            return (Versioning.version_major == 1
+            		&& Versioning.version_minor == 5);
 
             /*-----------------------------------------------*\
             | IMPLEMENTERS SHOULD NOT EDIT BEYOND THIS POINT! |
